2d2d72ca075b4ccd0e55e179dee3709accd19223
tranjames267 <58448725+tranjames267@users.noreply.github.com>
2021-12-08 16:08:52 -0700

    Added Personal Portfolio

---
+++ Portfolio/index.js
@@ -0,0 +1,15 @@
+let displaydetails = false;
+document.getElementById('contactBtn').addEventListener("click", showDetails);
+
+function showDetails(){
+  document.getElementById('details').style.display ="block";
+  document.getElementById('detailsoverlay').style.display = "block";
+  document.getElementById('detailsoverlay').addEventListener('click', clickedOverlay);
+  displaydetails = true;
+}
+
+function clickedOverlay(){
+  document.getElementById('details').style.display ="none";
+  document.getElementById('detailsoverlay').style.display = "none";
+  displaydetails = false;
+}
